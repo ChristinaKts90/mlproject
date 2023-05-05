0d2e5ee6a67621843db79530e37134b03238b513
Christinadidi10 <christinakatsiafa@yahoo.com>
2023-05-05 10:15:59 +0100

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,52 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+#from src.components.data_transformation import
+
+@dataclass
+class DataIngestionConfig:
+    logging.info("Setting the data paths")
+    try:
+        train_path: str=os.path.join("artifacts","train.csv")
+        test_path: str=os.path.join("artifacts","test.csv")
+        raw_path: str=os.path.join("artifacts","raw.csv")
+        logging.info("Setting of the data paths completed")
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv("notebook\data\StudentsPerformance.csv")
+            logging.info("Read the dataset as dataframe completed")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_path),exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_path, index=False, header=True)
+            
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_path, index=False, header=True)
+            logging.info("Ingestion of the data is completed")
+            
+            return(
+                self.ingestion_config.train_path,
+                self.ingestion_config.test_path,
+                self.ingestion_config.raw_path
+            )
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    
